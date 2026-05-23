840a1abbb2a872020ef51a32ee58b9a8bd5cec81
djb78 <djb78@protonmail.com>
2026-05-22 20:54:19 -0500

    add LeafNode class with basic unit tests

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -17,3 +17,17 @@ class HTMLNode():
     
     def __repr__(self):
         return f"tag:{self.tag}\nvalue:{self.value}\nchildren:{self.children}\nprops:{self.props}"
+
+class LeafNode(HTMLNode):
+    def __init__(self, tag, value, props=None):
+        super().__init__(tag, value, None, props)
+
+    def to_html(self):
+        if not self.value:
+            raise ValueError
+        if not self.tag:
+            return self.value
+        return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
+    
+    def __repr__(self):
+        return f"tag:{self.tag}\nvalue:{self.value}\nprops:{self.props}"
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,5 +1,5 @@
 import unittest
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
 class TestHTMLNode(unittest.TestCase):
     '''
@@ -23,6 +23,10 @@ class TestHTMLNode(unittest.TestCase):
     def test_empty_prop_html(self):
         node = HTMLNode("p", "hello", None, {})
         self.assertEqual(node.props_to_html(), '')
+
+    def test_leaf_to_html_p(self):
+        node = LeafNode("p", "Hello, world!")
+        self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
 
 if __name__ == "__main__":
     unittest.main()
