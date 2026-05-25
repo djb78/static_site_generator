e71dc618bc52a86232f2faef31c311108f6e6770
djb78 <djb78@protonmail.com>
2026-05-25 07:15:48 -0500

    use ValueError instead of Exception in split_nodes_delimiter

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -8,11 +8,9 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
         else:
             split_nodes = node.text.split(delimiter)
             if len(split_nodes) % 2 == 0:
-                raise Exception(f"Invalid markdown syntax: no closing {delimiter} within \"{node.text}\"")
+                raise ValueError(f"Invalid markdown syntax: no closing {delimiter} within \"{node.text}\"")
             for i in range(len(split_nodes)):
                 if split_nodes[i]:
                     new_type = node.text_type if i % 2 == 0 else text_type
                     new_nodes.append(TextNode(split_nodes[i], new_type))
     return new_nodes
-
-
