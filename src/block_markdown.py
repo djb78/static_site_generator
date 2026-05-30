cf83ad453b6225c457c07deb3eb24bf2f6002912
djb78 <djb78@protonmail.com>
2026-05-30 13:18:46 -0500

    add BlockType enum and block_to_blocktype function with basic unit tests

--- src/block_markdown.py
+++ src/block_markdown.py
@@ -1,7 +1,16 @@
+# import re
+from enum import Enum
 
+class BlockType(Enum):
+    PARAGRAPH = "pparagraph"
+    HEADING = "heading"
+    QUOTE = "quote"
+    CODE = "code"
+    UNORDERED_LIST = "unordered_list"
+    ORDERED_LIST = "ordered_list"
 
 def markdown_to_blocks(markdown):
-    # input: raw markdown string, full document [well-writen]
+    # markdown: raw markdown string, full document [well-writen]
     # return ["block strings"]
     blocks = []
     parts = markdown.split('\n\n')
@@ -9,3 +18,22 @@ def markdown_to_blocks(markdown):
         if part == '': continue
         blocks.append(part.strip())
     return blocks
+
+def block_to_blocktype(block):
+    # block: string representing a single block w/o leading/trailing whitespace
+    # return BlockType
+
+    # heading
+    first_word = block.split(' ', 1)[0]
+    if 1 <= len(first_word) <= 6 and all(c == '#' for c in first_word): return BlockType.HEADING
+    # code
+    if block.startswith("```") and '\n' in block and block.endswith("```"): return BlockType.CODE
+    # quote
+    lines = block.splitlines()
+    if all(line.startswith(">") for line in lines): return BlockType.QUOTE
+    # unordered list
+    if all(line.startswith("- ") for line in lines): return BlockType.UNORDERED_LIST
+    # ordered list
+    if all(line.startswith(f"{i}. ") for i, line in enumerate(lines, start=1)): return BlockType.ORDERED_LIST
+    # paragraph
+    return BlockType.PARAGRAPH
--- src/test_block_markdown.py
+++ src/test_block_markdown.py
@@ -1,9 +1,13 @@
 import unittest
 from block_markdown import (
-    markdown_to_blocks    
+    BlockType,
+    markdown_to_blocks,
+    block_to_blocktype    
 )
 
 class TestMarkdownToBlocks(unittest.TestCase):
+# markdown_to_blocks
+# ==================
     def test_markdown_to_blocks_multiple(self):
         md = """
 This is **bolded** paragraph
@@ -73,6 +77,79 @@ This is the same paragraph on a new line
             blocks,
             [],
         )
+
+# block_to_blocktype
+# ==================
+    def test_block_to_head(self):
+        block = "## this is a heading"
+        type = block_to_blocktype(block)
+        self.assertEqual(type, BlockType.HEADING)
+
+    def test_block_to_code(self):
+        block = "```\nthis is a code block\n```"
+        type = block_to_blocktype(block)
+        self.assertEqual(type, BlockType.CODE)
+
+    def test_block_to_quote(self):
+        block = ">this is a quote"
+        type = block_to_blocktype(block)
+        self.assertEqual(type, BlockType.QUOTE)
+
+    def test_block_to_ul(self):
+        block = "- this\n- list\n- is\n- unordered"
+        type = block_to_blocktype(block)
+        self.assertEqual(type, BlockType.UNORDERED_LIST)
+
+    def test_block_to_ol(self):
+        block = "1. this\n2. list\n3. is\n4. ordered"
+        type = block_to_blocktype(block)
+        self.assertEqual(type, BlockType.ORDERED_LIST)
+
+    def test_block_to_paragraph(self):
+        block = "this is a really short paragraph. if not for this sentence some may even just call it a sentence."
+        type = block_to_blocktype(block)
+        self.assertEqual(type, BlockType.PARAGRAPH)
+
+    def test_block_to_head_7(self):
+        block = "####### that's too many #'s"
+        type = block_to_blocktype(block)
+        self.assertEqual(type, BlockType.PARAGRAPH)
+
+    def test_block_to_head_6(self):
+        block = "###### smallest recognizable heading"
+        type = block_to_blocktype(block)
+        self.assertEqual(type, BlockType.HEADING)
+
+    def test_block_to_head_nospace(self):
+        block = "##I need some space here"
+        type = block_to_blocktype(block)
+        self.assertEqual(type, BlockType.PARAGRAPH)
+
+    def test_block_to_quote_multiline(self):
+        block = "> I might be wrong\n>but i'm not lying."
+        type = block_to_blocktype(block)
+        self.assertEqual(type, BlockType.QUOTE)
+
+    def test_block_to_quote_multiline_missing(self):
+        block = ">I might be wrong\nbut i'm not lying"
+        type = block_to_blocktype(block)
+        self.assertEqual(type, BlockType.PARAGRAPH)
+
+    def test_block_to_ol_skipone(self):
+        block = "2. list\n3. is\n4. ordered"
+        type = block_to_blocktype(block)
+        self.assertEqual(type, BlockType.PARAGRAPH)
+
+    def test_block_to_ol_skiptwo(self):
+        block = "1. this\n3. is\n4. ordered"
+        type = block_to_blocktype(block)
+        self.assertEqual(type, BlockType.PARAGRAPH)
+
+    def test_block_to_code_malformed(self):
+        block = "```"
+        type = block_to_blocktype(block)
+        self.assertEqual(type, BlockType.PARAGRAPH)
+    
 
 if __name__ == "__main__":
     unittest.main()
