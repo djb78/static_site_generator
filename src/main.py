dde48b5af097eb3f730d033494185d143cd2dc52
djb78 <djb78@protonmail.com>
2026-06-01 15:26:23 -0500

    add copy_directory function

--- src/main.py
+++ src/main.py
@@ -1,10 +1,30 @@
+import os, shutil
 from textnode import TextNode
 
-print("hello world")
+def copy_directory(source, destination):
+    # make sure source is valid
+    if not os.path.exists(source):
+        raise Exception(f"invalid directory: {source}")
+    if os.path.exists(destination):
+        shutil.rmtree(destination)
+    os.mkdir(destination)
+    
+    for child in os.listdir(source):
+        child_path = os.path.join(source, child)
+       
+        if os.path.isfile(child_path):
+            # copy to destination
+            shutil.copy(child_path, destination)
+        else:
+            # assume directory
+            destination_path = os.path.join(destination, child)
+            os.mkdir(destination_path)
+            copy_directory(child_path, destination_path)
 
 def main():
     node = TextNode("dummy text", "link", "https://wtf.com")
     print(node)
+    copy_directory("static", "public")
 
 if __name__ == "__main__":
     main()
