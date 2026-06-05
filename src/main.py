d297f3d94b0fdcc198b9a300162fe929ff80a531
djb78 <djb78@protonmail.com>
2026-06-04 19:37:38 -0500

    add generate_page calls for sample content

--- src/main.py
+++ src/main.py
@@ -51,6 +51,10 @@ def generate_page(from_path, template_path, dest_path):
 def main():
     copy_directory("static", "public")
     generate_page("content/index.md", "template.html", "public/index.html")
+    generate_page("content/contact/index.md", "template.html", "public/contact/index.html")
+    generate_page("content/blog/glorfindel/index.md", "template.html", "public/blog/glorfindel/index.html")
+    generate_page("content/blog/majesty/index.md", "template.html", "public/blog/majesty/index.html")
+    generate_page("content/blog/tom/index.md", "template.html", "public/blog/tom/index.html")
 
 if __name__ == "__main__":
     main()
