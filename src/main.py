e5b104ecacbf6b237e92021a87059069b971430d
djb78 <djb78@protonmail.com>
2026-06-05 20:26:20 -0500

    Add support for GitHub Pages base path

--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
-import os, shutil
+import os, shutil, sys
 from markdown_to_html import markdown_to_html_node
 from block_markdown import extract_title
 
@@ -22,7 +22,7 @@ def copy_directory(source, destination):
             os.mkdir(destination_path)
             copy_directory(child_path, destination_path)
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, basepath):
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
     markdown_file = template = ""
     try:
@@ -38,7 +38,7 @@ def generate_page(from_path, template_path, dest_path):
     
     title = extract_title(markdown_file)
     html_file = markdown_to_html_node(markdown_file).to_html()
-    page = template.replace("{{ Title }}", title).replace("{{ Content }}", html_file)
+    page = template.replace("{{ Title }}", title).replace("{{ Content }}", html_file).replace('href="/', f'href="{basepath}').replace('src="/', f'src="{basepath}')
 
     directory = os.path.dirname(dest_path)
     os.makedirs(directory, exist_ok=True)
@@ -48,7 +48,7 @@ def generate_page(from_path, template_path, dest_path):
     except FileNotFoundError:
         raise Exception(f"could not write to {dest_path}")
     
-def generate_pages_recursive(content_dir_path, template_path, dest_dir_path):
+def generate_pages_recursive(content_dir_path, template_path, dest_dir_path, basepath):
     files_and_folders = os.listdir(content_dir_path)
     # for each file/folder
     for file_or_folder in files_and_folders:
@@ -56,16 +56,19 @@ def generate_pages_recursive(content_dir_path, template_path, dest_dir_path):
         if os.path.isfile(file_or_folder_path):
             # if file generate page
             dest_path = os.path.join(dest_dir_path, file_or_folder.split('.')[0]+'.html')
-            generate_page(file_or_folder_path, template_path, dest_path)
+            generate_page(file_or_folder_path, template_path, dest_path, basepath)
         else: # if folder recurse
             dest_path = os.path.join(dest_dir_path, file_or_folder)
-            generate_pages_recursive(file_or_folder_path, template_path, dest_path)
+            generate_pages_recursive(file_or_folder_path, template_path, dest_path, basepath)
 
 
 
 def main():
+    
+    basepath = sys.argv[1] if sys.argv[1] else '/'
+    
     copy_directory("static", "public")
-    generate_pages_recursive('content', 'template.html', 'public')
+    generate_pages_recursive('content', 'template.html', 'public', basepath)
 
 if __name__ == "__main__":
     main()
